6167cda4dd8b4446cc6a3555a6fe356aab24df81
TheDuggy <gkollegger5@outlook.com>
2024-08-26 21:35:56 +0200

    Enhanced visual-mode.

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -1,8 +1,15 @@
+local sil = { silent = true }
+
 -- writing to file
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>i')
 
 -- copy / pasting
+vim.keymap.set('i', '<C-v>', '<Esc>"*pi', sil)
+vim.keymap.set('v', '<C-c>', '"+y<CR>i', sil)
 
-vim.keymap.set('i', '<C-v>', '<Esc>"*pi', { silent = true })
-vim.keymap.set('v', '<C-c>', '"+y<CR>i', { silent = false })
+-- enhanced visual-mode
+vim.keymap.set('v', 'i', '<up>', sil)
+vim.keymap.set('v', 'k', '<down>', sil)
+vim.keymap.set('v', 'j', '<left>', sil)
+vim.keymap.set('v', 'l', '<right>', sil)
