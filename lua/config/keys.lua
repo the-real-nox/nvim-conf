2eff583d37e2f9715334d7e9366f3cddc2ec204e
TheDuggy <gkollegger5@outlook.com>
2024-08-26 22:08:51 +0200

    Added other shortcuts.

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -8,8 +8,24 @@ vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>i')
 vim.keymap.set('i', '<C-v>', '<Esc>"*pi', sil)
 vim.keymap.set('v', '<C-c>', '"+y<CR>i', sil)
 
--- enhanced visual-mode
-vim.keymap.set('v', 'i', '<up>', sil)
-vim.keymap.set('v', 'k', '<down>', sil)
-vim.keymap.set('v', 'j', '<left>', sil)
-vim.keymap.set('v', 'l', '<right>', sil)
+-- enhanced movement
+for i,v in ipairs({ 'n', 'v' }) do
+    vim.keymap.set(v, 'i', '<up>', sil)
+    vim.keymap.set(v, 'k', '<down>', sil)
+    vim.keymap.set(v, 'j', '<left>', sil)
+    vim.keymap.set(v, 'l', '<right>', sil)
+end
+
+-- modes
+vim.keymap.set('', '<A-c>', '<Esc>', sil)
+vim.keymap.set('', '<A-i>', '<Esc>:startinsert<CR>', sil)
+vim.keymap.set('', '<A-v>', '<Esc>:normal! v<CR>', sil)
+vim.keymap.set('v', '<BS>', '<del>', sil)
+
+-- misc
+vim.keymap.set('', '<C-q>', function ()
+    local quit = vim.fn.input('Quit all? ')
+    if quit == 'y' then
+        vim.cmd('qa!')
+    end
+end, sil)
