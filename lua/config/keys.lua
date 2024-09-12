e0f52f74b40f6d8295b2bda9b8afef14db1ebb04
koding-george <gkollegger5@outlook.com>
2024-09-12 15:01:09 +0200

    Added bufferline-indicators, removed enhanced (bad) movement

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -8,21 +8,7 @@ vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>i')
 vim.keymap.set('i', '<C-v>', '<Esc>"*pi', sil)
 vim.keymap.set('v', '<C-c>', '"+y<CR>i', sil)
 
--- enhanced movement
-for i,v in ipairs({ 'n', 'v' }) do
-    vim.keymap.set(v, 'i', '<up>', sil)
-    vim.keymap.set(v, 'k', '<down>', sil)
-    vim.keymap.set(v, 'j', '<left>', sil)
-    vim.keymap.set(v, 'l', '<right>', sil)
-end
-
 -- modes
-vim.keymap.set('', '<S-M-d>', '<Esc>', sil)
-vim.keymap.set('', '<S-M-i>', '<Esc>:startinsert<CR>', sil)
-vim.keymap.set('', '<S-M-v>', '<Esc>:normal! v<CR>', sil)
-vim.keymap.set('v', '<BS>', '<del>', sil)
-
--- misc
 vim.keymap.set('', '<C-q>', function ()
     local quit = vim.fn.input('Quit all? ')
     if quit == 'y' then
--- lua/config/misc.lua
+++ lua/config/misc.lua
@@ -18,13 +18,17 @@ options = {
             separator = true
         }
     },
-
     custom_filter = function(buf_number, buf_numbers)
         local filetype = vim.bo[buf_number].filetype
         if filetype == "neo-tree" then
             return false
         end
         return true
+    end,
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local icon = level:match("error") and " " or " "
+        return " " .. icon .. count
     end
     }
 }
